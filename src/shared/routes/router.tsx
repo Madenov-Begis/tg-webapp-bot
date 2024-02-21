f69160113b981d1a742d37fced9284988e9ade67
Madenov-Begis <begismadenov@gmail.com>
2024-02-21 22:15:24 +0500

    route fix

--- src/shared/routes/router.tsx
+++ src/shared/routes/router.tsx
@@ -1,25 +1,25 @@
-import { Outlet, createBrowserRouter } from 'react-router-dom'
+import {  createBrowserRouter } from 'react-router-dom'
 import HomePage from '@/pages/home-page'
 import ProductDetail from '@/pages/product-detail'
 import Cart from '@/pages/cart'
 
 export const router = createBrowserRouter([
+  // {
+  //   path: '/:locale',
+  //   element: <Outlet />,
+  //   children: [
   {
-    path: '/:locale',
-    element: <Outlet />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'product',
-        element: <ProductDetail />,
-      },
-      {
-        path: 'cart',
-        element: <Cart />,
-      },
-    ],
+    path: '/',
+    element: <HomePage />,
   },
+  {
+    path: '/product',
+    element: <ProductDetail />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  //   ],
+  // },
 ])
