1b967c54997bb7324a168b604abe96a2f94ae74a
b.madenov <b.madenov@uzinfocom.uz>
2025-06-30 19:05:58 +0500

    feat: enhance Telegram WebApp integration with fullscreen support and improved closing confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
+import { useEffect } from 'react'
+import { RouterProvider } from 'react-router-dom'
 import { router } from './shared/routes/router'
 import { Container } from './shared/ui/container/container'
-import { RouterProvider } from 'react-router-dom'
-import { useEffect } from 'react'
 
 const App = () => {
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
+    //@ts-expect-error
     const tg = window?.Telegram?.WebApp
 
     if (tg) {
       // Разворачиваем приложение на всю высоту
       tg.expand()
 
+      if (tg.requestFullscreen) {
+        tg.requestFullscreen()
+      }
+
+      // ВАЖНО: Отключаем закрытие через свайп/скролл
+      tg.disableClosingConfirmation()
+
+      // Включаем подтверждение закрытия - теперь закроется только через кнопку
+      tg.enableClosingConfirmation()
+
+      // Отключаем возможность закрытия свайпом вниз
+      if (tg.isClosingConfirmationEnabled !== undefined) {
+        tg.enableClosingConfirmation()
+      }
+
+      // Блокируем вертикальные свайпы для закрытия
+      if (tg.disableVerticalSwipes) {
+        tg.disableVerticalSwipes()
+      }
+
       // Ставим цвет фона и шапки
       tg.setHeaderColor('#000000')
       tg.setBackgroundColor('#000000')
-
-      // Включаем защиту от закрытия
-      tg.enableClosingConfirmation?.()
-      tg.disableVerticalSwipes?.()
 
       // Отмечаем, что готово
       tg.ready()
@@ -38,12 +54,10 @@ const App = () => {
   }, [])
 
   return (
-    <main className="relative max-w-[600px] mx-auto min-h-screen">
-      <div className="bg-white mt-12 min-h-[calc(100vh-3rem)]">
-        <Container>
-          <RouterProvider router={router} />
-        </Container>
-      </div>
+    <main className="max-w-[600px] mx-auto min-h-screen pt-12">
+      <Container>
+        <RouterProvider router={router} />
+      </Container>
     </main>
   )
 }
