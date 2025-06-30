cc8daebab097b31137eafdc669a41f9b907bb4a9
b.madenov <b.madenov@uzinfocom.uz>
2025-06-30 18:45:33 +0500

    refactor: streamline Telegram WebApp integration and update main layout styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,16 @@ import { useEffect } from 'react'
 const App = () => {
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-expect-error
+    // @ts-expect-error
     const tg = window.Telegram?.WebApp
 
     if (tg) {
-      // Расширяем приложение до полной высоты
       tg.expand()
-
-      // Включаем полноэкранный режим (если доступен)
-      if (tg.requestFullscreen) {
-        tg.requestFullscreen()
-      }
-
-      // ВАЖНО: Отключаем закрытие через свайп/скролл
-      tg.disableClosingConfirmation()
-
-      // Включаем подтверждение закрытия - теперь закроется только через кнопку
       tg.enableClosingConfirmation()
-
-      // Отключаем возможность закрытия свайпом вниз
-      if (tg.isClosingConfirmationEnabled !== undefined) {
-        tg.enableClosingConfirmation()
-      }
-
-      // Блокируем вертикальные свайпы для закрытия
-      if (tg.disableVerticalSwipes) {
-        tg.disableVerticalSwipes()
-      }
-
-      // Устанавливаем цвет заголовка
       tg.setHeaderColor('#000000')
-
-      // Устанавливаем цвет фона
       tg.setBackgroundColor('#000000')
-
-      // Показываем приложение как готовое к использованию
       tg.ready()
 
-      // Дополнительная защита от случайного закрытия
       const handleBeforeUnload = (e: BeforeUnloadEvent) => {
         e.preventDefault()
         return ''
@@ -51,7 +23,6 @@ const App = () => {
 
       window.addEventListener('beforeunload', handleBeforeUnload)
 
-      // Очистка при размонтировании
       return () => {
         window.removeEventListener('beforeunload', handleBeforeUnload)
       }
@@ -59,7 +30,7 @@ const App = () => {
   }, [])
 
   return (
-    <main className="relative w-full min-h-screen bg-black">
+    <main className="relative max-w-[600px] mx-auto min-h-screen pt-12">
       <div className="bg-white mt-12 min-h-[calc(100vh-3rem)]">
         <Container>
           <RouterProvider router={router} />
