a545cd4306b2e25e00ce96387d74f15ea2f73f16
b.madenov <b.madenov@uzinfocom.uz>
2025-06-30 18:23:39 +0500

    feat: add Telegram WebApp expand functionality on app load

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { router } from './shared/routes/router'
 import { Container } from './shared/ui/container/container'
 import { RouterProvider } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const App = () => {
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-expect-error
+    if (window.Telegram?.WebApp?.expand) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-expect-error
+      window.Telegram.WebApp.expand()
+    }
+  }, [])
+
   return (
     <main className="relative max-w-[600px] mx-auto h-full bg-white">
       <Container>
